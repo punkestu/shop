542f55e40e79d0e4f34a6a6f91a4208ea165d734
Dobberman <pangestubima89@gmail.com>
2024-03-10 15:56:23 +0700

    cart: create invoice popup

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -2,6 +2,7 @@
 	import { cart } from '$lib/store/cart';
 	import { onMount } from 'svelte';
 	import Products from '$lib/data/products.json';
+	import Shade from '../../components/Shade.svelte';
 
 	/**
 	 * @type {any[]}
@@ -12,12 +13,19 @@
 	 */
 	let readyToCheckout = [];
 	let total = 0;
+	let paymentIsOpen = false;
 	cart.subscribe((value) => {
 		cartProducts = value;
 	});
 	onMount(() => {
 		cartProducts = JSON.parse(localStorage.getItem('cart') || '[]');
 	});
+
+	function checkoutPopup() {
+		if (readyToCheckout.length > 0) {
+			paymentIsOpen = true;
+		}
+	}
 
 	function checkout() {
 		cart.update((cart) => {
@@ -26,8 +34,31 @@
 		});
 		localStorage.setItem('cart', JSON.stringify(cartProducts));
 		readyToCheckout = [];
+		total = 0;
+		paymentIsOpen = false;
 	}
 </script>
+
+<Shade bind:isOpen={paymentIsOpen}>
+	<div class="animate-popup bg-slate-50 z-50 text-slate-900 p-4 w-1/4 flex flex-col">
+		<h2 class="text-2xl font-bold">Payment</h2>
+		<div class="flex flex-col gap-4">
+			<ul>
+				{#each readyToCheckout as productID (productID)}
+					<li class="flex items-center gap-4">
+						<p>{Products.find((p) => p.id === productID)?.name}</p>
+						<p>
+							${(cartProducts.find((p) => p.id === productID)?.qty || 0) *
+								(Products.find((p) => p.id === productID)?.price || 0)}
+						</p>
+					</li>
+				{/each}
+			</ul>
+			<p>Total: ${total}</p>
+			<button class="bg-slate-800 text-slate-50 px-4 py-2" on:click={checkout}>Pay</button>
+		</div>
+	</div>
+</Shade>
 
 <section class="px-4 pt-6 md:pt-12 pb-2 flex flex-col">
 	{readyToCheckout.length} Selected
@@ -44,10 +75,10 @@
 					on:change={() => {
 						if (readyToCheckout.includes(product.id)) {
 							readyToCheckout = readyToCheckout.filter((productId) => productId !== product.id);
-							total -= Products.find((p) => p.id === product.id)?.price || 0 * product.qty;
+							total -= (Products.find((p) => p.id === product.id)?.price || 0) * product.qty;
 						} else {
 							readyToCheckout = [...readyToCheckout, product.id];
-							total += Products.find((p) => p.id === product.id)?.price || 0 * product.qty;
+							total += (Products.find((p) => p.id === product.id)?.price || 0) * product.qty;
 						}
 					}}
 				/>
@@ -62,6 +93,6 @@
 	{/each}
 	<div class="flex items-center gap-4">
 		<p>Total: ${total}</p>
-		<button class="bg-slate-800 px-4 py-2 flex-grow" on:click={checkout}>Checkout</button>
+		<button class="bg-slate-800 px-4 py-2 flex-grow" on:click={checkoutPopup}>Checkout</button>
 	</div>
 </section>
